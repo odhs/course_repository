8e05700b4b80de6ac5e87099fd9093ac6dfa2350
Sérgio Oliveira <sergiohe2@gmail.com>
2024-08-26 17:10:44 -0300

    Sharing-room button added

--- semana-tech-01-go-react/semana-tech-01-go-react-web/src/pages/room.tsx
+++ semana-tech-01-go-react/semana-tech-01-go-react-web/src/pages/room.tsx
@@ -7,7 +7,15 @@ export function Room() {
   // Return all parameters from URL
   const { roomId } = useParams();
 
- 
+  function handleShareRoom() {
+    const url = window.location.href.toString();
+
+    if (navigator.share != undefined && navigator.canShare()) {
+      navigator.share({ url });
+    } else {
+      navigator.clipboard.writeText(url);
+    }
+  }
 
   return (
     <div className="mx-auto max-w-[640px] flex flex-col gap-6 py-10 px-4">
@@ -20,6 +28,7 @@ export function Room() {
 
         <button
           type="submit"
+          onClick={handleShareRoom}
           className="ml-auto bg-zinc-800 text-zinc-300 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-zinc-700"
         >
           Compartilhar
@@ -29,47 +38,47 @@ export function Room() {
 
       <div className="h-px w-full bg-zinc-900" />
 
-      <form        
-        className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1">
-          <input
-            type="text"
-            name="theme"
-            placeholder="Qual a sua pergunta?"
-            autoComplete="off"
-            className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
-          />
+      <form className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1">
+        <input
+          type="text"
+          name="theme"
+          placeholder="Qual a sua pergunta?"
+          autoComplete="off"
+          className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
+        />
 
+        <button
+          type="submit"
+          className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500"
+        >
+          Criar Pergunta
+          <ArrowRight className="size-4" />
+        </button>
+      </form>
+
+      <ol className="list-decimal list-inside px-3 space-y-8">
+        <li className="ml-4 leading-relaxed text-zinc-100">
+          Text
           <button
-            type="submit"
-            className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500"
+            type="button"
+            className="mt-3 flex items-center gap-2 text-orange-400 text-sm font-medium hover:text-orange-500"
           >
-            Criar Pergunta
-            <ArrowRight className="size-4" />
+            <ArrowUp className="size-4" />
+            Curtir pergunta (123)
           </button>
-        </form>
+        </li>
 
-        <ol className="list-decimal list-inside px-3 space-y-8">
-          <li className="ml-4 leading-relaxed text-zinc-100">
-            Text
-
-            <button type="button" className="mt-3 flex items-center gap-2 text-orange-400 text-sm font-medium hover:text-orange-500">
-              <ArrowUp className="size-4"/>
-               Curtir pergunta (123)
-            </button>
-          </li>
-
-
-          <li className="ml-4 leading-relaxed text-zinc-100">
-            Text
-
-            <button type="button" className="mt-3 flex items-center gap-2 text-zinc-400 text-sm font-medium hover:text-zinc-300">
-              <ArrowUp className="size-4"/>
-               Curtir pergunta (123)
-            </button>
-          </li>
-
-        </ol>
-
+        <li className="ml-4 leading-relaxed text-zinc-100">
+          Text
+          <button
+            type="button"
+            className="mt-3 flex items-center gap-2 text-zinc-400 text-sm font-medium hover:text-zinc-300"
+          >
+            <ArrowUp className="size-4" />
+            Curtir pergunta (123)
+          </button>
+        </li>
+      </ol>
     </div>
   );
 }
