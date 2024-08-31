7c2fc318d75fda8385bf89278577fe5e2e841075
Sérgio Oliveira <6653128+odhs@users.noreply.github.com>
2024-08-30 22:13:56 -0300

    Added WebSocket connections pool

--- semana-tech-01-go-react/semana-tech-01-go-react-server/internal/api/api.go
+++ semana-tech-01-go-react/semana-tech-01-go-react-server/internal/api/api.go
@@ -1,20 +1,34 @@
 package api
 
 import (
+	"context"
+	"errors"
+	"log/slog"
 	"net/http"
+	"sync"
 
 	"odhs/semana-tech-01-go-react-server-main/internal/store/pgstore"
 
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+
+	"github.com/gorilla/websocket"
 )
 
 type apiHandler struct {
 	// Concrete mode with pgstore, because this app is simple,
 	// not necessary an interface to attach another database if necessary.
-	q *pgstore.Queries
-	r *chi.Mux
+	q        *pgstore.Queries
+	r        *chi.Mux
+	upgrader websocket.Upgrader
+	// Pool de Conexões para clientes conectados via WebSocket
+	// map[roomId] por map[clientes conectados]
+	subscribers map[string]map[*websocket.Conn]context.CancelFunc
+	// Blocks data race
+	mu *sync.Mutex
 }
 
 // Call the handler ServeHTTP inside the Chi framework
@@ -25,6 +39,11 @@ func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func NewHandler(q *pgstore.Queries) http.Handler {
 	apiH := apiHandler{
 		q: q,
+		upgrader: websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool { return true },
+		},
+		subscribers: make(map[string]map[*websocket.Conn]context.CancelFunc),
+		mu:          &sync.Mutex{},
 	}
 
 	// It could also be NewMux, NewRouter is the same as NewMux
@@ -72,7 +91,75 @@ func NewHandler(q *pgstore.Queries) http.Handler {
 	return apiH
 }
 
-func (h apiHandler) handleSubscribe(w http.ResponseWriter, r *http.Request)              {}
+/*
+handleSubscribe
+- It is a function that blocks execution because it maintains the connection
+with the user open, waiting the user or the server cancel it. So we use the
+customer flag to cancel the context, canceling the context,
+we canceled the connection.
+*/
+func (h apiHandler) handleSubscribe(w http.ResponseWriter, r *http.Request) {
+	// Get and check for valid room ID
+	rawRoomID := chi.URLParam(r, "room_id")
+	roomID, err := uuid.Parse(rawRoomID)
+	if err != nil {
+		http.Error(w, "invalid room id", http.StatusBadRequest)
+	}
+
+	// Verify if room exists
+	_, err = h.q.GetRoom(r.Context(), roomID)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			http.Error(w, "room not found", http.StatusBadRequest)
+			return
+		}
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	// Until now the connection is a normal HTTP Request,
+	// Let's transform this connection in a websocket
+	c, err := h.upgrader.Upgrade(w, r, nil)
+	// Let's verify why the cliente is not able to upgrade
+	if err != nil {
+		slog.Warn("failed to upgrade connection", "error", err)
+		http.Error(w, "failed to upgrade to ws connection", http.StatusBadRequest)
+		return
+	}
+	defer c.Close()
+
+	// Contexto com a função cancel, para poder liberar a função caso a conexão
+	// seja interrompida.
+	ctx, cancel := context.WithCancel(r.Context())
+
+	// Atualizando o mapa de clientes com o novo subscriber que acabou de chegar
+	// Se ainda não existir o websocket da sala cria
+	// Se já existir apenas atribui a função "cancel".
+	h.mu.Lock()
+	if _, ok := h.subscribers[rawRoomID]; !ok {
+		h.subscribers[rawRoomID] = make(map[*websocket.Conn]context.CancelFunc)
+	}
+	slog.Info(
+		"new cliente connected",
+		"room_id", rawRoomID,
+		"cliente_ip",
+		r.RemoteAddr,
+	)
+	h.subscribers[rawRoomID][c] = cancel
+	h.mu.Unlock()
+
+	// O programa ficará aguardando aqui a conexão ser cancelada
+	// ou pelo cliente ou pelo servidor.
+	<-ctx.Done()
+
+	// Quando o programa chegar aqui quer dizer que a conexão foi cancelada
+	// Então vamos retirar a conexão do pool de conexões
+	h.mu.Lock()
+	delete(h.subscribers[rawRoomID], c)
+	h.mu.Unlock()
+}
+
 func (h apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request)             {}
 func (h apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request)               {}
 func (h apiHandler) handleCreateRoomMessage(w http.ResponseWriter, r *http.Request)      {}
